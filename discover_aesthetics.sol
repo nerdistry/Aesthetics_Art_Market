798d6a1520fb42ed75b0e60c0c8db010e33c1a42
Tevin-Isaac <isaactevin@yahoo.com>
2023-02-22 17:53:10 +0300

    updated smart contract with new functions

--- discover_aesthetics.sol
+++ discover_aesthetics.sol
@@ -1,6 +1,5 @@
-
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.8.3;
 
 interface IERC20Token {
   function transfer(address, uint256) external returns (bool);
@@ -9,126 +8,133 @@ interface IERC20Token {
   function totalSupply() external view returns (uint256);
   function balanceOf(address) external view returns (uint256);
   function allowance(address, address) external view returns (uint256);
-
   event Transfer(address indexed from, address indexed to, uint256 value);
   event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-contract discover_aesthetics {
 
-    uint internal media_length = 0; // Represents how many art products stored and can be used as an index of next art.
+contract Cameo{
+
+    //track the number of arts stored
+    uint internal listedArtLength;
+
+    //cUSD token address
     address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
-    address public seller_address;
 
-    struct art_medium{
-        address payable owner; // Allows my contract to send tokens to this address.
-        /*String because they will never be negative.*/
-        string art_type;
+
+    //struct to hold art details
+    struct artInfo{
+        address payable owner;
         string name;
-        string image;
-        string description;
-        bool licensed;
-        string location;
+        string ImgUrl;
+        string Details;
+        string Location;
         uint price;
-        uint sold;
+        string email;
     }
 
-    modifier is_licensed(uint _index){
-        require(art_media[_index].licensed == true, "This masterpiece has not been licensed.");
-        _;
-        
-    }
 
-    modifier art_owner(){
-        require(seller_address == msg.sender,"Modification rights revoked, you are not the owner. ");
-        _;
-    }
-
-    mapping (uint => art_medium) public art_media;
-
-    function write_art_medium(
-        // uint _index,
-        string memory _art_type,
-        string memory _name,
-        string memory _image,
-        string memory _description,
-        string memory _location,
-        uint _price
-    ) public {
-        require(bytes(_art_type).length > 0, "Kindly specify the type of art.");
-        require(bytes(_name).length > 0, "Your masterpiece should have a name.");
-        require(bytes(_image).length > 0, "Kindly input a valid URL.");
-        require(bytes(_description).length > 10, "Kindly give a detailed description of your art.");
-        require(_price > 0, "Kindly input a valid price.");
-        uint _sold = 0; // Tracks the number of times the art product was sold.
-        bool _licensed = false;
+//store purchased arts
+struct purchasedArt{
+    address From;
+    string name;
+    string imgUrl;
+    uint timestamp;
+    uint price;
+    string email;
+}
 
 
-        art_media[media_length] = art_medium(
-            payable(msg.sender), // Returns the address of the entity that is making the call.
-            _art_type,
-            _name,
-            _image,
-            _description,
-            _licensed,
-            _location,
-            _price,
-            _sold
+//store all the listed arts
+mapping(uint => artInfo) internal listedArts;
+
+//store purchased arts
+mapping(address => purchasedArt[]) internal purchasedArts;
+
+//modifier for onlyOwner
+modifier onlyOwner(uint _index){
+    require(msg.sender == listedArts[_index].owner,"You are not authorized");
+    _;
+}
+
+
+//store  art in the smart contract
+function listArt(
+    string calldata _name,
+    string calldata _ImgUrl,
+    string calldata _details,
+    string calldata _location,
+    uint _price,
+    string calldata _email
+) public {
+    require(bytes(_name).length > 0, "name cannot be empty");
+    require(bytes(_ImgUrl).length > 0, "url cannot be empty");
+    require(bytes(_details).length > 0, "details cannot be empty");
+    require(bytes(_location).length > 0, "location cannot be empty");
+    require(bytes(_email).length > 0, "email cannot be empty");
+    require(_price > 0, "Price is invalid");
+
+    listedArts[listedArtLength] = artInfo(
+        payable(msg.sender),
+        _name,
+        _ImgUrl,
+        _details,
+        _location,
+        _price,
+        _email
         );
-        media_length++;
-    }
+        listedArtLength++;
+}
+ 
+//get  art with specific id
+function getSpecificArt(uint _index) public view returns(artInfo memory){
+    return listedArts[_index];
+}
 
-    function read_art_medium(uint _index) public view returns (
-        address payable,
-        string memory,
-        string memory,
-        string memory,
-        string memory,
-        bool
-     
-    ) {
-        return (
-            art_media[_index].owner,
-            art_media[_index].art_type,
-            art_media[_index].name,
-            art_media[_index].image,
-            art_media[_index].description,
-            art_media[_index].licensed
-       
+//Buy art 
+function  buyArt(uint _index) public payable {
+    artInfo memory art = listedArts[_index];
+    require(msg.sender != art.owner,"You are already the owner");
+
+    require(
+          IERC20Token(cUsdTokenAddress).transferFrom(
+            msg.sender,
+            art.owner,
+            art.price
+          ),
+          "Transfer failed."
         );
-    }
+        purchasedArts[msg.sender].push(purchasedArt(
+            art.owner,
+            art.name,
+            art.ImgUrl,
+            block.timestamp,
+            art.price,
+            art.email
+        ));
+        art.owner = payable(msg.sender);
+}
 
-    // Trying to solve the stack too deep error.
+//Retreive art purchased by a specific buyer
+function getMyArts() public view returns(purchasedArt[] memory){
+    return purchasedArts[msg.sender];
+}
 
-    function read_another_art_medium(uint _index) public view returns (
-  
-        string memory,
-        uint, 
-        uint
-    ) {
-        return (
-        
-            art_media[_index].location,
-            art_media[_index].price,
-            art_media[_index].sold
-        );
-    }
+//get listed art length
+function artLength() public view returns(uint){
+    return listedArtLength;
+}
 
-    function buy_masterpiece(uint _index) public payable{
-                require(art_media[_index].owner != address(0), "Please input a valid index!");
+//Edit the art price
+function EditPrice(uint _index, uint _price) public onlyOwner(_index){
+    require(_price > 0,"Price can not be zero");
+    listedArts[_index].price = _price;
+}
 
-        require(
-             IERC20Token(cUsdTokenAddress).transferFrom(
-			msg.sender,
-            art_media[_index].owner,
-            art_media[_index].price
-        ),
-        "Transaction has failed, sorry."
-        );
-        art_media[_index].sold++;
-    }
+//delete art from store
+function deleteArt(uint _index) public onlyOwner(_index){
+    delete listedArts[_index];
+}
 
-    function get_media_length() public view returns (uint){
-        return (media_length);
-    }
+
 }
